a364d03b08b5c44272add27ccc8a37a19a52ff81
binarybaba <amin.ajani17@gmail.com>
2018-04-18 21:48:38 +0530

    demo for babel

--- js/messageController.js
+++ js/messageController.js
@@ -1,7 +1,20 @@
 angular.module("app")
 .controller("messageController", ["$scope", "messageFactory", function($scope, MessageF) {
     MessageF.getMessage()
-        .then(function(response) {
+        .then((response) => {
             $scope.message = response.data.message;
         })
 }]);
+class Foo {
+    constructor(x) {
+        this.x = x;
+        this.y = 432;
+    }
+
+    point() {
+        return 'Foo(' + this.x + ', ' + this.y + ')';
+    }
+}
+
+
+
